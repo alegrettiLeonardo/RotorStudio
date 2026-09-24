8d93ee89c10a31a63e1ddc9520449b531923495f
Leonardo Alegretti <64331203+alegrettiLeonardo@users.noreply.github.com>
2026-09-24 12:07:35 -0300

    fix: correct Python Core facade module newlines

--- python/src/drm_core/persistence/__init__.py
+++ python/src/drm_core/persistence/__init__.py
@@ -1,1 +1,2 @@
-from drm_core.stage1 import save_project,load_project\n__all__=['save_project','load_project']\n
+from drm_core.stage1 import save_project, load_project
+__all__ = ["save_project", "load_project"]
