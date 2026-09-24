53e027d6f1ee7ac929d01710b89a022008e39110
Leonardo Alegretti <64331203+alegrettiLeonardo@users.noreply.github.com>
2026-09-24 19:43:45 -0300

    ui21: export bearing performance workspace

--- python/src/drm_studio/widgets/__init__.py
+++ python/src/drm_studio/widgets/__init__.py
@@ -1,3 +1,5 @@
 from .rotor_view import RotorView, RotorModelPage
 from .analysis_modules import AnalysisModulesBar
-__all__ = ["RotorView", "RotorModelPage", "AnalysisModulesBar"]
+from .bearing_performance import BearingPerformancePage
+
+__all__ = ["RotorView", "RotorModelPage", "AnalysisModulesBar", "BearingPerformancePage"]
