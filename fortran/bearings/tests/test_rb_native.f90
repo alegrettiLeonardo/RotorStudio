9dcf203422f81b72f97a16a158812a286339638b
Leonardo Alegretti <64331203+alegrettiLeonardo@users.noreply.github.com>
2026-09-25 09:16:49 -0300

    test: qualify fixed geometry and tilting-pad configuration

--- fortran/bearings/tests/test_rb_native.f90
+++ fortran/bearings/tests/test_rb_native.f90
@@ -5,6 +5,9 @@ program test_ross_bearings_native
   use rb_rolling, only: rb_ball_coefficients, rb_roller_coefficients
   use rb_cylindrical, only: rb_cylindrical_coefficients
   use rb_squeeze_film_damper, only: rb_sfd_coefficients, RB_SFD_GROOVE_END_SEALS
+  use rb_fixed_geometry, only: rb_partial_arc_geometry, rb_elliptical_geometry, rb_offset_halves_geometry, &
+                               rb_multi_lobe_geometry, rb_pressure_dam_geometry, rb_plain_journal_geometry
+  use rb_tilting_pad_config, only: rb_tilting_pad_prepare, RB_TP_CONVENTIONAL, RB_TP_MATCH_LOAD
   implicit none(type, external)
 
   integer(ik) :: st
@@ -16,9 +19,11 @@ program test_ross_bearings_native
   call test_interpolation()
   call test_rolling()
   call test_cylindrical()
+  call test_fixed_geometry()
+  call test_tilting_pad_config()
   call test_sfd()
 
-  print *, 'PASS standalone ROSS bearing native gates BF1/BF2/BF3/BF7'
+  print *, 'PASS standalone ROSS bearing native gates BF1/BF2/BF3/BF4/BF5cfg/BF7'
 
 contains
 
@@ -124,6 +129,84 @@ contains
                                      ms, som, ecc, att, K, C, st)
     if (st /= RB_ERR_INPUT) error stop 314
   end subroutine test_cylindrical
+
+  subroutine test_fixed_geometry()
+    integer(ik) :: np
+    real(rk) :: pivot2(2), arc2(2), pre2(2), off2(2)
+    real(rk) :: track_arc(2), track_len(2), track_depth(2)
+    real(rk) :: pre3(3), off3(3), pivot3(3), arc3(3)
+    real(rk) :: p1(1), a1(1), pr1(1), o1(1)
+    real(rk) :: pj_pivot(4), pj_arc(4), pj_pre(4), pj_off(4), pj_len(4), pj_thick
+
+    call rb_partial_arc_geometry(pi_/2._rk, 3._rk*pi_/2._rk, 0._rk, 0.5_rk, &
+                                 np, p1, a1, pr1, o1, st)
+    if (st /= RB_OK .or. np /= 1) error stop 351
+    call assert_close(p1(1), 3._rk*pi_/2._rk, 1e-14_rk, 1e-14_rk, 352)
+
+    call rb_elliptical_geometry(150._rk*pi_/180._rk, 0.5_rk, np, pivot2, arc2, pre2, off2, st)
+    if (st /= RB_OK .or. np /= 2) error stop 353
+    call assert_close(pivot2(1), pi_/2._rk, 1e-14_rk, 1e-14_rk, 354)
+    call assert_close(pivot2(2), 3._rk*pi_/2._rk, 1e-14_rk, 1e-14_rk, 355)
+    call assert_close(pre2(1), 0.5_rk, 1e-14_rk, 1e-14_rk, 356)
+    call assert_close(off2(2), 0.5_rk, 1e-14_rk, 1e-14_rk, 357)
+
+    call rb_offset_halves_geometry(150._rk*pi_/180._rk, 0.4_rk, 0.6_rk, np, &
+                                   pivot2, arc2, pre2, off2, st)
+    if (st /= RB_OK .or. np /= 2) error stop 358
+    call assert_close(off2(1), 0.6_rk, 1e-14_rk, 1e-14_rk, 359)
+
+    pre3 = [0.3_rk, 0.4_rk, 0.5_rk]
+    off3 = [0.45_rk, 0.5_rk, 0.55_rk]
+    call rb_multi_lobe_geometry(3_ik, 100._rk*pi_/180._rk, pre3, off3, 0._rk, .false., &
+                                pivot3, arc3, pre3, off3, st)
+    if (st /= RB_OK) error stop 360
+    call assert_close(pivot3(1), pi_/3._rk, 1e-14_rk, 1e-14_rk, 361)
+    call assert_close(pivot3(2), pi_, 1e-14_rk, 1e-14_rk, 362)
+    call assert_close(pivot3(3), 5._rk*pi_/3._rk, 1e-14_rk, 1e-14_rk, 363)
+
+    call rb_pressure_dam_geometry(150._rk*pi_/180._rk, pi_/2._rk, 0.1_rk, 250e-6_rk, &
+                                  .true., .false., 0._rk, np, pivot2, arc2, pre2, off2, &
+                                  track_arc, track_len, track_depth, st)
+    if (st /= RB_OK .or. np /= 2) error stop 364
+    call assert_close(track_arc(1), pi_/2._rk, 1e-14_rk, 1e-14_rk, 365)
+    call assert_close(track_arc(2), 0._rk, 1e-14_rk, 1e-14_rk, 366)
+
+    call rb_plain_journal_geometry(4_ik, 80._rk*pi_/180._rk, 0.1_rk, 0.05_rk, 0.2_rk, &
+                                   0._rk, .false., pj_pivot, pj_arc, pj_pre, pj_off, pj_len, pj_thick, st)
+    if (st /= RB_OK) error stop 367
+    call assert_close(pj_pivot(1), pi_/4._rk, 1e-14_rk, 1e-14_rk, 368)
+    call assert_close(pj_pivot(4), 7._rk*pi_/4._rk, 1e-14_rk, 1e-14_rk, 369)
+    call assert_close(pj_thick, 0.05_rk, 1e-14_rk, 1e-14_rk, 370)
+  end subroutine test_fixed_geometry
+
+  subroutine test_tilting_pad_config()
+    real(rk) :: pivot(5), arc(5), plen(5), pre(5), off(5), pos(2)
+
+    pivot = [18._rk,90._rk,162._rk,234._rk,306._rk] * pi_/180._rk
+    arc = 60._rk*pi_/180._rk
+    plen = 50.8e-3_rk
+    pre = 0.5_rk
+    off = 0.5_rk
+
+    call rb_tilting_pad_prepare(5_ik,101.6e-3_rk,74.9e-6_rk,12.7e-3_rk, &
+                                pivot,arc,plen,pre,off,RB_TP_CONVENTIONAL,RB_TP_MATCH_LOAD, &
+                                0.3_rk,3._rk*pi_/2._rk,.false.,0._rk,0._rk,20_ik,10_ik,10_ik,pos,st)
+    if (st /= RB_OK) error stop 371
+    call assert_close(pos(1), 0._rk, 1e-14_rk, 1e-14_rk, 372)
+    call assert_close(pos(2), -0.3_rk, 1e-14_rk, 1e-14_rk, 373)
+
+    call rb_tilting_pad_prepare(5_ik,101.6e-3_rk,74.9e-6_rk,12.7e-3_rk, &
+                                pivot,arc,plen,pre,off,RB_TP_CONVENTIONAL,RB_TP_MATCH_LOAD, &
+                                0.3_rk,3._rk*pi_/2._rk,.true.,0.12_rk,-0.21_rk,20_ik,10_ik,10_ik,pos,st)
+    if (st /= RB_OK) error stop 374
+    call assert_close(pos(1), 0.12_rk, 1e-14_rk, 1e-14_rk, 375)
+    call assert_close(pos(2), -0.21_rk, 1e-14_rk, 1e-14_rk, 376)
+
+    call rb_tilting_pad_prepare(5_ik,101.6e-3_rk,74.9e-6_rk,12.7e-3_rk, &
+                                pivot,arc,plen,pre,off,RB_TP_CONVENTIONAL,RB_TP_MATCH_LOAD, &
+                                0.3_rk,3._rk*pi_/2._rk,.false.,0._rk,0._rk,21_ik,10_ik,10_ik,pos,st)
+    if (st /= RB_ERR_INPUT) error stop 377
+  end subroutine test_tilting_pad_config
 
   subroutine test_sfd()
     real(rk), parameter :: reyn_to_pas = 6894.757293168_rk
